c13ccbdf8760a415f06d5f6ceb0b6eabbfb32059
Thibaut C. <20838877+Onirim@users.noreply.github.com>
2026-04-02 19:38:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //   - Images Supabase → Network First avec fallback cache
 // ══════════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'Hirondelles-v15';
+const CACHE_NAME = 'Hirondelles-v16';
 
 // Assets mis en cache dès l'installation
 const PRECACHE_ASSETS = [
